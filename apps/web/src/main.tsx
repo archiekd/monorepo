959b9a7bf25e9193556d109a6eb0d151a962c62e
Archie Kennedy-Dyson <archiekd@gmail.com>
2022-08-14 23:06:05 +0100

    Remove unnecessary imports

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,8 @@
 import { StrictMode } from "react"
+
 import * as ReactDOM from "react-dom/client"
 
-import { ApolloProvider } from "@apollo/client"
-import { theme } from "@monorepo/ui"
-import { createTheme } from "@mui/material"
-import { ThemeProvider } from "styled-components"
-
 import App from "./app/App"
-import { client } from "./app/graphql"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
