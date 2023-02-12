2240d7ac90a20eedf2e6112dc618a32a4b3a66bd
Hector Kennedy-Dyson <hectorekd@gmail.com>
2023-02-12 13:38:51 +0000

    adjust colors

--- libs/ui/src/components/CreateRoutine/StartValueBox/StartValueBox.tsx
+++ libs/ui/src/components/CreateRoutine/StartValueBox/StartValueBox.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Box } from "@mui/material"
+import { Box, useTheme } from "@mui/material"
 
 import { UiTypography } from "../../ui"
 import { displayNumWith1Decimal } from "./helper"
@@ -16,14 +16,15 @@ export type StartValueBoxProps = {
 }
 
 export const StartValueBox: React.FC<StartValueBoxProps> = ({ base, dScore, requirements, connections, totalStart }) => {
+  const theme = useTheme()
   return (
     <StartValueWrapper>
       <ScoreAddition label="Base - " score={base} color="black" />
-      <ScoreAddition label="D score - " score={dScore} color="purple" />
-      <ScoreAddition label="Requirements - " score={requirements} color="green" />
-      <ScoreAddition label="Connections - " score={connections} color="orange" />
+      <ScoreAddition label="D score - " score={dScore} color="#dc5687" />
+      <ScoreAddition label="Requirements - " score={requirements} color="#56dc68" />
+      <ScoreAddition label="Connections - " score={connections} color="#dcab56" />
       <Box
-        sx={{ backgroundColor: "lightblue", height: "60px" }}
+        sx={{ backgroundColor: theme.palette.primary.light, height: "60px" }}
         borderRadius="0 0 5px 5px"
         display="flex"
         justifyContent="center"
